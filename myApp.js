a372720f86ca3fc64fecc04ba0cea3e04457e423
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 19:54:00 +0000

    implement queryChain

--- myApp.js
+++ myApp.js
@@ -107,8 +107,11 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, data) => done(err, data));
 };
 
 /** **Well Done !!**
