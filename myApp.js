f2b72bcbd9aee3878e185eb0748bba69abfd95f3
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 18:38:51 +0000

    add mongodb connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-
+require("dotenv").config();
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
