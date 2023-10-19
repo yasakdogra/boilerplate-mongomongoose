f07831af9a9999f9cfad0148c829860ed948f2d6
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 19:18:23 +0000

    implement findPersonById

--- myApp.js
+++ myApp.js
@@ -50,7 +50,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function (err, data) {
+    if (err) done(err);
+    else done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
