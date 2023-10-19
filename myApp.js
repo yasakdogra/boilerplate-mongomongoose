d52c7c1decf7f0239ef1ecb2f3b504d0ee40baf5
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 19:15:23 +0000

    implement findOneByFood

--- myApp.js
+++ myApp.js
@@ -43,7 +43,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) done(err);
+    else done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
